4a1eed435ec544c782809a6af9f48bc0ec8eba84
Johanna Fagerås <fagerasjohanna1@gmail.com>
2026-01-22 01:46:20 +0100

    ..

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,24 @@
-import adapter from '@sveltejs/adapter-static';
+import adapterVercel from '@sveltejs/adapter-vercel';
+import adapterStatic from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const isVercel = process.env.VERCEL === '1';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
-			// Output directory for the static build (Capacitor will use this)
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html', // SPA fallback for client-side routing
-			precompress: false,
-			strict: true
-		})
+		adapter: isVercel
+			? adapterVercel()
+			: adapterStatic({
+					// Output directory for the static build (Capacitor will use this)
+					pages: 'build',
+					assets: 'build',
+					fallback: 'index.html', // SPA fallback for client-side routing
+					precompress: false,
+					strict: true
+				})
 	}
 };
 
