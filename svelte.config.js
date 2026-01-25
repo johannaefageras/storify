bcee0d0aeb7011248a1f016998bd1b31ba464253
Johanna Fagerås <fagerasjohanna1@gmail.com>
2026-01-25 13:53:21 +0100

    ..

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,17 @@
-import adapterVercel from '@sveltejs/adapter-vercel';
+import adapterNode from '@sveltejs/adapter-node';
 import adapterStatic from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const isVercel = process.env.VERCEL === '1';
+// Render sets RENDER=true automatically
+const isRender = process.env.RENDER === 'true';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: isVercel
-			? adapterVercel()
+		adapter: isRender
+			? adapterNode()
 			: adapterStatic({
 					// Output directory for the static build (Capacitor will use this)
 					pages: 'build',
